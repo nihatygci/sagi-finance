a4d0ba06e95a4de0b8c695d6494101fd6c7f0109
nihat <139617369+nihatygci@users.noreply.github.com>
2026-04-21 14:18:07 +0300

    Deactive

--- sw.js
+++ sw.js
@@ -1,3 +1,4 @@
+/*
 // SAGI Finance Service Worker - v2
 const CACHE_NAME = "sagi-cache-v2";
 const RUNTIME_CACHE = "sagi-runtime-v1";
@@ -111,3 +112,4 @@ self.addEventListener('notificationclick', function(event) {
       })
   );
 });
+*/
